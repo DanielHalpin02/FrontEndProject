dfdb93b765368c7e2ebd496bb10c2ccc8ecd3390
DanielHalpin02 <G00436639@atu.ie>
2026-04-23 16:21:01 +0100

    Added workout search feature

--- src/app/workouts/workouts.page.ts
+++ src/app/workouts/workouts.page.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
+  IonBackButton,
+  IonButtons,
   IonCard,
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
   IonContent,
   IonHeader,
+  IonSearchbar,
   IonTitle,
-  IonToolbar,
-  IonBackButton,
-  IonButtons
+  IonToolbar
 } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 
@@ -24,29 +25,48 @@ import { WorkoutService } from '../services/workout.service';
   standalone: true,
   imports: [
     CommonModule,
+    IonBackButton,
+    IonButtons,
     IonCard,
     IonCardContent,
     IonCardHeader,
     IonCardTitle,
     IonContent,
     IonHeader,
+    IonSearchbar,
     IonTitle,
     IonToolbar,
-    IonBackButton,
-    IonButtons,
     RouterLink
   ]
 })
 export class WorkoutsPage implements OnInit {
   workouts: Workout[] = [];
+  filteredWorkouts: Workout[] = [];
   errorMessage = '';
+  searchTerm = '';
 
   constructor(private workoutService: WorkoutService) {}
 
   ngOnInit(): void {
     this.workoutService.getWorkouts().subscribe({
-      next: (data) => (this.workouts = data),
-      error: () => (this.errorMessage = 'Unable to load workouts right now.')
+      next: (data) => {
+        this.workouts = data;
+        this.filteredWorkouts = data;
+      },
+      error: () => {
+        this.errorMessage = 'Unable to load workouts right now.';
+      }
     });
+  }
+
+  onSearchChange(event: Event): void {
+    const target = event.target as HTMLIonSearchbarElement;
+    const value = target.value?.toLowerCase() || '';
+
+    this.searchTerm = value;
+
+    this.filteredWorkouts = this.workouts.filter(workout =>
+      workout.name.toLowerCase().includes(value)
+    );
   }
 }
