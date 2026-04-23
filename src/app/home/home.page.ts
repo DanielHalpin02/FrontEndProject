87205e66c47dfdfecce61efde728f0ea955b37fe
DanielHalpin02 <G00436639@atu.ie>
2026-04-23 13:41:15 +0100

    Configure Ionic Storage in app bootstrap

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; 
-import { IonButton, IonContent, IonHeader, IonTitle, IonToolbar, IonRouterLink } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonRouterLink]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink]
 })
 export class HomePage {}
