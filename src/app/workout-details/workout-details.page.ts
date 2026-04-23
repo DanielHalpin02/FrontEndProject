3858455776bebed6592b5d0f03ced27d98b459b2
DanielHalpin02 <G00436639@atu.ie>
2026-04-23 15:02:10 +0100

    Built workout details page with descriptions

--- src/app/workout-details/workout-details.page.ts
+++ src/app/workout-details/workout-details.page.ts
@@ -1,20 +1,71 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+import { Workout } from '../models/workout';
+import { WorkoutService } from '../services/workout.service';
 
 @Component({
   selector: 'app-workout-details',
   templateUrl: './workout-details.page.html',
   styleUrls: ['./workout-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonCardHeader,
+    IonCardTitle,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    RouterLink
+  ]
 })
 export class WorkoutDetailsPage implements OnInit {
+  workout: Workout | undefined;
+  description = '';
+  errorMessage = '';
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private workoutService: WorkoutService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    const workoutId = this.route.snapshot.paramMap.get('id');
+
+    if (!workoutId) {
+      this.errorMessage = 'Workout not found.';
+      return;
+    }
+
+    this.workoutService.getWorkoutById(workoutId).subscribe({
+      next: (data) => {
+        if (!data) {
+          this.errorMessage = 'Workout not found.';
+          return;
+        }
+
+        this.workout = data;
+        this.description = this.workoutService.getWorkoutDescription(data.name);
+      },
+      error: () => {
+        this.errorMessage = 'Unable to load workout details right now.';
+      }
+    });
   }
-
 }
